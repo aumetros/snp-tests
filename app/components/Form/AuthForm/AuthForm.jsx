017b48ce8965ccd87cf37d34289483533d21a49b
aumetros <aum3tros@yandex.ru>
2024-02-05 23:17:25 +0300

    Add LoginPage: создана форма для входа в приложение

--- app/components/Form/AuthForm/AuthForm.jsx
+++ app/components/Form/AuthForm/AuthForm.jsx
@@ -1,6 +1,6 @@
 import { Form } from 'components';
 import { Input, InputError, Button, Typography, Checkbox } from 'ui';
-import { AUTH, AUTH_CHECK, ERROR } from 'utils/constants/modes';
+import { AUTH, AUTH_CHECK, ERROR, SIGNUP } from 'utils/constants/modes';
 import { PAR2, BLUE_DEEP } from 'utils/constants/variants';
 import styles from './AuthForm.module.scss';
 
@@ -22,24 +22,29 @@ export default function AuthForm({ mode }) {
 					mode={AUTH}
 				/>
 				<InputError variant={ERROR}></InputError>
-				<Input
-					type="text"
-					name="password_confirmation"
-					placeholder={'Повторите пароль'}
-					mode={AUTH}
-				/>
-				<InputError variant={ERROR}></InputError>
-				<div className={styles['admin']}>
-					<div className={styles['admin__container']}>
-						<Checkbox name="is_admin" mode={AUTH_CHECK} />
-						<Typography variant={PAR2} color={BLUE_DEEP}>
-							Вы являетесь администратором?
-						</Typography>
-					</div>
-					<InputError variant={ERROR}></InputError>
-				</div>
+				{mode === SIGNUP && (
+					<>
+						<Input
+							type="text"
+							name="password_confirmation"
+							placeholder={'Повторите пароль'}
+							mode={AUTH}
+						/>
+						<InputError variant={ERROR}></InputError>
+						<div className={styles['admin']}>
+							<div className={styles['admin__container']}>
+								<Checkbox name="is_admin" mode={AUTH_CHECK} />
+								<Typography variant={PAR2} color={BLUE_DEEP}>
+									Вы являетесь администратором?
+								</Typography>
+							</div>
+							<InputError variant={ERROR}></InputError>
+						</div>
+					</>
+				)}
+
 				<Button type="submit" mode={AUTH}>
-					Зарегистрировать
+					{mode === SIGNUP ? 'Зарегистрировать' : 'Войти'}
 				</Button>
 			</div>
 		</Form>
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,22 @@
-import { Main } from "components";
+import { Main, Section, AuthForm } from 'components';
+import { Typography } from 'ui';
+import { H2 } from 'utils/constants/variants';
+import { SIGNIN } from 'utils/constants/modes';
+import styles from "./LoginPage.module.scss";
 
 export default function LoginPage() {
-  return (
-   <Main>
-
-   </Main>
-  );
+	return (
+		<Main>
+			<Section>
+				<div className={styles['container']}>
+					<Typography variant={H2}>Войти в приложение</Typography>
+				</div>
+			</Section>
+			<Section>
+				<div className={styles['container']}>
+					<AuthForm mode={SIGNIN} />
+				</div>
+			</Section>
+		</Main>
+	);
 }
